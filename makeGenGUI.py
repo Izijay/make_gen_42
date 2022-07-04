6e73ee98c4d5f769e3e8572238b47f94d867507b
izijay <michael_dupuis@orange.fr>
2022-07-04 15:57:19 +0200

    fix indentation mais ca marche pas avec les wildcards

--- makeGenGUI.py
+++ makeGenGUI.py
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    make_gen.py                                        :+:      :+:    :+:    #
+#    makeGenGUI.py                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: mdupuis <mdupuis@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/09 19:03:29 by mdupuis           #+#    #+#              #
-#    Updated: 2022/04/07 13:03:20 by mdupuis          ###   ########.fr        #
+#    Updated: 2022/07/04 15:30:41 by mdupuis          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,11 +14,12 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 import os
+import glob
 
 # Create Window
 root = Tk()
 root.title("Makefile Generator")
-root.geometry("545x635")
+root.geometry("430x600")
 root.resizable(False, False)
 root.config(bg='#4065A4')
 
@@ -83,6 +84,7 @@ def reset():
     entry_bin_bonus.delete(0, END)
     entry_src_bonus.delete(0, END)
     entry_inc_bonus.delete(0, END)
+    entry_project_path.delete(0, END)
 
 
 def quit_after_generate():
@@ -224,10 +226,23 @@ entry_inc_bonus = Entry(frame, textvariable=entry_inc_bonus, bg='white', fg='bla
                         disabledbackground='grey')
 entry_inc_bonus.grid(row=13, column=1, sticky=W)
 
+# Absolute path to project:
+entry_project_path = StringVar()
+entry_project_path.set("ex: /mnt/nfs/homes/login/.../")
+Label(frame, text="Absolute path to project:\t", bg='#eeeeee', fg='black', font=("Calibri", 12, "bold")).grid(row=14,
+                                                                                                          column=0,
+                                                                                                          sticky=W)
+entry_project_path = Entry(frame, textvariable=entry_project_path, bg='white', fg='black', font=('Helvetica', 12),
+                        disabledbackground='grey')
+entry_project_path.grid(row=14, column=1, sticky=W)
+
 
 def generate():
     """
     Generate the Makefile
+    """
+    """TO DO:
+    - !!! get all srcs filenames in src directory to replace wildcards !!!
     """
     # Get the values of the radio buttons:
     if language.get() == "C":
@@ -263,7 +278,14 @@ def generate():
         if bin_bonus == "" or src_bonus == "" or inc_bonus == "":
             messagebox.showwarning("Error", "Please fill all the entries")
             return
-
+    absolute_path = entry_project_path.get()
+    if absolute_path == "":
+        messagebox.showwarning("Error", "Please fill all the entries")
+        return
+    with os.scandir() as i:
+        for entry in i:
+         if entry.is_file():
+              print(entry.name)
     # Create the Makefile:
     f = filedialog.asksaveasfile(title="Save as...", initialdir=os.getcwd(), mode="w")
     if f is None:
@@ -272,7 +294,7 @@ def generate():
     if bonus.get() == "y":
         f.write("\n\nNAME_BONUS\t\t=\t" + bin_bonus)
     if lang == "C":
-        f.write("\n\nCC\t\t\t\t=\tgcc")
+        f.write("\n\nCC\t\t\t=\tgcc")
     else:
         f.write("\n\nCXX\t\t\t\t=\tc++")
     f.write("\n\nSRC_DIR\t\t\t=\t$(shell find " + src + " -type d)")
@@ -294,16 +316,16 @@ def generate():
     if bonus.get() == "y":
         f.write("\nvpath %.c $(foreach dir, $(SRC_BONUS_DIR), $(dir):)")
     f.write("\n\n# library -----------------------------------------------------------")
-    f.write("\n\nSRC\t\t\t\t=\t$(wildcard " + src + "/*.c*)\n\n")
+    f.write("\n\nSRC\t\t\t=\t$(wildcard " + src + "/*.c*)\n\n")
     if bonus.get() == "y":
-        f.write("SRC_BONUS\t\t=\t$(wildcard " + src_bonus + "/*.c*)\n\n")
-    f.write("INC\t\t\t\t=\t$(wildcard " + inc + "/*.h*)\n\n")
+        f.write("SRC_BONUS\t=\t$(wildcard " + src_bonus + "/*.c*)\n\n")
+    f.write("INC\t\t\t=\t$(wildcard " + inc + "/*.h*)\n\n")
     if bonus.get() == "y":
         f.write("INC_BONUS\t\t=\t$(wildcard " + inc_bonus + "/*.h*)\n\n")
     if language.get() == "C":
-        f.write("OBJ\t\t\t\t=\t$(addprefix $(OBJ_DIR)/, $(SRC:%.c=%.o))\n\n")
+        f.write("OBJ\t\t\t=\t$(addprefix $(OBJ_DIR)/, $(SRC:%.c=%.o))\n\n")
     else:
-        f.write("OBJ\t\t\t\t=\t$(addprefix $(OBJ_DIR)/, $(SRC:%.cpp=%.o))\n\n")
+        f.write("OBJ\t\t\t=\t$(addprefix $(OBJ_DIR)/, $(SRC:%.cpp=%.o))\n\n")
     if bonus.get() == "y":
         if language.get() == "C":
             f.write("OBJ_BONUS\t\t=\t$(addprefix $(OBJ_BONUS_DIR)/, $(SRC_BONUS:%.c=%.o))\n\n")
@@ -311,31 +333,31 @@ def generate():
             f.write("OBJ_BONUS\t\t=\t$(addprefix $(OBJ_BONUS_DIR)/, $(SRC_BONUS:%.cpp=%.o))\n\n")
     f.write("# Compilation flags -------------------------------------------------\n\n")
     if lang == "C":
-        f.write("CFLAGS\t\t\t=\t-Wall -Wextra -Werror\n\n")
+        f.write("CFLAGS\t\t\t=\t-Wall -Wextra -Werror\n")
     else:
-        f.write("CPPFLAGS\t\t\t=\t-Wall -Wextra -Werror -std=c++98\n\n")
+        f.write("CPPFLAGS\t\t\t=\t-Wall -Wextra -Werror -std=c++98\n")
     if libft.get() == "y" and mlx.get() == "n":
-        f.write("IFLAGS\t\t\t=	-I " + inc + "/ -I " + libft_ok + "/includes -I /usr/include\n\n")
+        f.write("IFLAGS\t\t=	-I " + inc + "/ -I " + libft_ok + "/includes -I /usr/include\n")
     elif mlx.get() == "y" and libft.get() == "y":
-        f.write("IFLAGS\t\t\t=	-I " + inc + "/ -I " + libft_ok + "/includes -I " + mlx_ok + " -I /usr/include\n\n")
+        f.write("IFLAGS\t\t=	-I " + inc + "/ -I " + libft_ok + "/includes -I " + mlx_ok + " -I /usr/include\n")
     elif mlx.get() == "y" and libft.get() == "n":
-        f.write("IFLAGS\t\t\t=	-I " + inc + "/ -I " + mlx_ok + " -I /usr/include\n\n")
+        f.write("IFLAGS\t\t=	-I " + inc + "/ -I " + mlx_ok + " -I /usr/include\n")
     else:
-        f.write("IFLAGS	\t\t\t=	-I" + inc + "-I /usr/include\n\n")
+        f.write("IFLAGS	\t\t=	-I " + inc + "/ -I /usr/include\n")
     if bonus.get() == "y":
         if libft.get() == "y" and mlx.get() == "n":
-            f.write("IFLAGS_B\t\t=	-I " + inc_bonus + " -I " + libft_ok + "/includes -I /usr/include\n\n")
+            f.write("IFLAGS_B\t\t=	-I " + inc_bonus + " -I " + libft_ok + "/includes -I /usr/include\n")
         elif mlx.get() == "y" and libft.get() == "y":
             f.write(
-                "IFLAGS_B\t\t=	-I " + inc_bonus + " -I " + libft_ok + "/includes -I " + mlx_ok + " -I /usr/include\n\n")
+                "IFLAGS_B\t\t=	-I " + inc_bonus + " -I " + libft_ok + "/includes -I " + mlx_ok + " -I /usr/include\n")
         elif mlx.get() == "y" and libft.get() == "n":
-            f.write("IFLAGS_B\t\t=	-I " + inc_bonus + " -I " + mlx_ok + " -I /usr/include\n\n")
+            f.write("IFLAGS_B\t\t=	-I " + inc_bonus + " -I " + mlx_ok + " -I /usr/include\n")
         else:
-            f.write("IFLAGS_B\t\t=	-I " + inc_bonus + " -I /usr/include\n\n")
+            f.write("IFLAGS_B\t\t=	-I " + inc_bonus + " -I /usr/include\n")
     if mlx.get() == "y":
-        f.write("LFLAGS\t\t\t=	-lmlx -lm -lX11 -lXext\n\n")
+        f.write("LFLAGS\t\t\t=	-lmlx -lm -lX11 -lXext\n")
     f.write("""
-# Colors ------------------------------------------------------------
+# Colors ------------------------------------------------------------\n
 _GREY	=	$'\\e[30m
 _RED	=	$'\\e[31m
 _GREEN	=	$'\\e[32m
@@ -345,34 +367,34 @@ _PURPLE	=	$'\\e[35m
 _CYAN	=	$'\\e[36m
 _WHITE	=	$'\\e[37m\n\n""")
     f.write("# main part ---------------------------------------------------------\n\n")
-    if language == "C":
+    if lang == "C":
         f.write("""
 $(OBJ_DIR)/%.o : %.c $(INC)
-            @echo "Compiling $(_YELLOW)$@$(_WHITE) ... \\c"
-            @mkdir -p $(OBJ_DIR)
-            $(CC) $(CFLAGS) $(IFLAGS) -c $< -o $@
-            @echo \"$(_GREEN)DONE$(_WHITE)\"\n""")
+\t\t@echo "Compiling $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t@mkdir -p $(OBJ_DIR)
+\t\t$(CC) $(CFLAGS) $(IFLAGS) -c $< -o $@
+\t\t@echo \"$(_GREEN)DONE$(_WHITE)\"\n""")
         if bonus.get() == "y":
             f.write("""
 $(OBJ_BONUS_DIR)/%.o : %.c $(INC_BONUS) 
-            @echo "Compiling $(_YELLOW)$@$(_WHITE) ... \\c"
-            @mkdir -p $(OBJ_BONUS_DIR)
-            $(CC) $(CFLAGS) $(IFLAGS_B) -c $< -o $@
-            @echo \"$(_GREEN)DONE$(_WHITE)\"\n""")
-    if language == "C++":
+\t\t@echo "Compiling $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t@mkdir -p $(OBJ_BONUS_DIR)
+\t\t$(CC) $(CFLAGS) $(IFLAGS_B) -c $< -o $@
+\t\t@echo \"$(_GREEN)DONE$(_WHITE)\"\n""")
+    if lang == "C++":
         f.write("""
 $(OBJ_DIR)/%.o : %.cpp $(INC)
-            @echo "Compiling $(_YELLOW)$@$(_WHITE) ... \\c"
-            @mkdir -p $(OBJ_DIR)
-            $(CXX) $(CPPFLAGS) $(IFLAGS) -c $< -o $@
-            @echo \"$(_GREEN)DONE$(_WHITE)\"\n""")
+\t\t@echo "Compiling $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t@mkdir -p $(OBJ_DIR)
+\t\t$(CXX) $(CPPFLAGS) $(IFLAGS) -c $< -o $@
+\t\t@echo \"$(_GREEN)DONE$(_WHITE)\"\n""")
         if bonus.get() == "y":
             f.write("""
 $(OBJ_BONUS_DIR)/%.o : %.cpp $(INC_BONUS) 
-            @echo "Compiling $(_YELLOW)$@$(_WHITE) ... \\c"
-            @mkdir -p $(OBJ_BONUS_DIR)
-            $(CXX) $(CPPFLAGS) $(IFLAGS_B) -c $< -o $@
-            @echo \"$(_GREEN)DONE$(_WHITE)\"\n""")
+\t\t@echo "Compiling $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t@mkdir -p $(OBJ_BONUS_DIR)
+\t\t$(CXX) $(CPPFLAGS) $(IFLAGS_B) -c $< -o $@
+\t\t@echo \"$(_GREEN)DONE$(_WHITE)\"\n""")
 
     if libft.get() == "y" and mlx.get() == "y":
         f.write("all: $(LIBFT_A) $(MLX_A) $(NAME)\n")
@@ -385,11 +407,11 @@ $(OBJ_BONUS_DIR)/%.o : %.cpp $(INC_BONUS)
     if libft.get() == "y":
         f.write("""
 $(LIBFT_A):
-            @make - C $(LIBFT)\n""")
+\t\t@make -C $(LIBFT)\n""")
     if mlx.get() == "y":
         f.write("""
 $(MLX_A):
-            @make - C $(MLX)\n""")
+\t\t@make -C $(MLX)\n""")
     if bonus.get() == "y":
         if libft.get() == "y" and mlx.get() == "y":
             f.write("\nbonus: $(LIBFT_A) $(MLX_A) $(NAME_BONUS)\n")
@@ -402,66 +424,66 @@ $(MLX_A):
     if libft.get() == "y" and mlx.get() == "y":
         f.write("""
 $(NAME): $(OBJ)
-        @echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
-        $(CC) $(CFLAGS) $(OBJ) $(IFLAGS) $(LIBFT_A) -L $(MLX) $(LFLAGS) -o $(NAME)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t$(CC) $(CFLAGS) $(OBJ) $(IFLAGS) $(LIBFT_A) -L $(MLX) $(LFLAGS) -o $(NAME)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
     elif libft.get() == "y" and mlx.get() == "n":
         f.write("""
 $(NAME): $(OBJ)
-        @echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
-        $(CC) $(CFLAGS) $(OBJ) $(IFLAGS) $(LIBFT_A) -o $(NAME)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t$(CC) $(CFLAGS) $(OBJ) $(IFLAGS) $(LIBFT_A) -o $(NAME)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
     elif libft.get() == "n" and mlx.get() == "y":
         f.write("""
 $(NAME): $(OBJ)
-        @echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
-        $(CC) $(CFLAGS) $(OBJ) $(IFLAGS) -L $MLX) $(LFLAGS) -o $(NAME)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t$(CC) $(CFLAGS) $(OBJ) $(IFLAGS) -L $MLX) $(LFLAGS) -o $(NAME)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
     else:
         f.write("""
 $(NAME): $(OBJ)
-        @echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
-        $(CC) $(CFLAGS) $(OBJ) $(IFLAGS) -o $(NAME)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t$(CC) $(CFLAGS) $(OBJ) $(IFLAGS) -o $(NAME)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
     if bonus.get() == "y":
         if libft.get() == "y" and mlx.get() == "y":
             f.write("""
 $(NAME_BONUS): $(OBJ_BONUS)
-        @echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
-        $(CC) $(CFLAGS) $(OBJ_BONUS) $(IFLAGS_B) $(LIBFT_A) -L $(MLX) $(LFLAGS) -o $(NAME_BONUS)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t$(CC) $(CFLAGS) $(OBJ_BONUS) $(IFLAGS_B) $(LIBFT_A) -L $(MLX) $(LFLAGS) -o $(NAME_BONUS)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
         elif libft.get() == "y" and mlx.get() == "n":
             f.write("""
 $(NAME_BONUS): $(OBJ_BONUS)
-        @echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
-        $(CC) $(CFLAGS) $(OBJ_BONUS) $(IFLAGS_B) $LIBFT_A) -o $(NAME_BONUS)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t$(CC) $(CFLAGS) $(OBJ_BONUS) $(IFLAGS_B) $LIBFT_A) -o $(NAME_BONUS)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
         elif libft.get() == "n" and mlx.get() == "y":
             f.write("""
 $(NAME_BONUS): $(OBJ_BONUS)
-        @echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
-        $(CC) $(CFLAGS) $(OBJ_BONUS) $(IFLAGS_B) -L $(MLX) $(LFLAGS) -o $(NAME_BONUS)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t$(CC) $(CFLAGS) $(OBJ_BONUS) $(IFLAGS_B) -L $(MLX) $(LFLAGS) -o $(NAME_BONUS)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
         else:
             f.write("""
 $(NAME_BONUS): $(OBJ_BONUS)
-        @echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
-        $(CC) $(CFLAGS) $(OBJ_BONUS) $(IFLAGS_B) -o $(NAME_BONUS)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "-----\\nCreating Binary File $(_YELLOW)$@$(_WHITE) ... \\c"
+\t\t$(CC) $(CFLAGS) $(OBJ_BONUS) $(IFLAGS_B) -o $(NAME_BONUS)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
     f.write("""
 clean:
-        @echo "$(_WHITE)Deleting Objects Directory $(_YELLOW)$(OBJ_DIR)$(_WHITE) ... \\c"
-        @rm -rf $(OBJ_DIR) $(OBJ_DIR_BONUS)
-        @make clean -C $(MLX)
-        @make clean -C $(LIBFT)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "$(_WHITE)Deleting Objects Directory $(_YELLOW)$(OBJ_DIR)$(_WHITE) ... \\c"
+\t\t@rm -rf $(OBJ_DIR) $(OBJ_DIR_BONUS)
+\t\t@make clean -C $(MLX)
+\t\t@make clean -C $(LIBFT)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
     f.write("""
 fclean: clean
-        @echo "$(_WHITE)Deleting Binary File $(_YELLOW)$(NAME)$(_WHITE) ... \\c"
-        @rm -f $(NAME) $(NAME_BONUS)
-        @echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
+\t\t@echo "$(_WHITE)Deleting Binary File $(_YELLOW)$(NAME)$(_WHITE) ... \\c"
+\t\t@rm -f $(NAME) $(NAME_BONUS)
+\t\t@echo "$(_GREEN)DONE$(_WHITE)\\n-----\"\n""")
     f.write("\nre:\t\tfclean all\n\n")
-    f.write(".PHONY:           all bonus clean fclean re\n")
+    f.write(".PHONY:\t\tall bonus clean fclean re\n")
     f.close()
     quit_after_generate()
 
@@ -469,10 +491,10 @@ fclean: clean
 # Buttons -------------------------------------------------------------------------------------------------------------:
 # Reset:
 Button(frame, text="Reset", bg='#eeeeee', fg='black', font=("Calibri", 12, "bold"), relief='ridge', borderwidth=4,
-       command=reset).grid(row=14, column=0,
+       command=reset).grid(row=15, column=0,
                            sticky=W, pady=20)
 # Generate:
-Button(frame, text="Generate", bg='#eeeeee', fg='black', font=("Calibri", 12, "bold"), command=generate).grid(row=14,
+Button(frame, text="Generate", bg='#eeeeee', fg='black', font=("Calibri", 12, "bold"), command=generate).grid(row=15,
                                                                                                               column=1,
                                                                                                               sticky=E,
                                                                                                               pady=20)
